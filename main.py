eaf764080f06dbdb3754f4867929a68c8b6faa95
wisdomjohnuti <johnuti101@gmail.com>
2025-12-07 23:57:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from scripts.scraper import get_over25_games
 from analyze import filter_top_games
 
-
 def run():
-    print("🔎 Scraping websites...")
+    print("🔎 Scraping...")
     data = get_over25_games()
 
-    print("📊 Analyzing data...")
+    print("DEBUG OUTPUT:", data)
+
+    print("📊 Analyzing...")
     top5, top10 = filter_top_games(data)
 
-    print("\n🔥 TOP 5 OVER 2.5 GAMES")
-    for game in top5:
-        print(game)
-
-    print("\n🔥 TOP 10 OVER 2.5 GAMES")
-    for game in top10:
-        print(game)
-
+    print("---- TOP 5 GAMES ----")
+    for g, s in top5:
+        print(g, s)
 
 if __name__ == "__main__":
     run()
